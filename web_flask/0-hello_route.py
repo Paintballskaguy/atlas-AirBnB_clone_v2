0e7d07fdd3aca517b722fb58c1f5b8be26e58c55
Paintballskaguy <Johnstournament@gmail.com>
2024-10-31 13:13:06 -0500

    task 0- testing Flask for the first time

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/bin/usr/python3
+
+"""
+This is the script for starting the Flask web app    
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return a greeting message"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
