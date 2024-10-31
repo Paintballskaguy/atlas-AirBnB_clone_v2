50a02c49f0ac41820ef28918fd5145c4f21a189e
Paintballskaguy <Johnstournament@gmail.com>
2024-10-31 13:21:30 -0500

    pycode style issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This is the script for starting the Flask web app    
+This is the script for starting the Flask web app
 """
 
 
@@ -9,10 +9,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return a greeting message"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
