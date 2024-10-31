8b0c7ea1897ead5664e52618c46d52328d392fe7
Paintballskaguy <Johnstournament@gmail.com>
2024-10-31 15:38:45 -0500

    blank line fixer

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,10 +15,12 @@ def hello_hbnb():
     """Return a greeting message"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB' at the /hbnb URL"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
