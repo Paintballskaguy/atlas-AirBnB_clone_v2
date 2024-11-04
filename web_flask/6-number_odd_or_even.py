2516acd1953e1430d4d3cd8a83192b8b2894f3b4
Paintballskaguy <Johnstournament@gmail.com>
2024-11-04 16:23:46 -0600

    task 6 - added the even or odd and an html that is basically copy and paste.

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""
+This is the script to show HBNB.
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return a greeting message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB' at the /hbnb URL"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays 'C' followed by value of text
+    replaces all the underscores with spaces.
+    """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Display 'Python ' followed by the value of the text variable
+    Replaces underscores with spaces. Default value is 'is cool'.
+    """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display '<n> is a number' only if n is an int"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display an HTML page only if n is an integer.
+    H1 tag: “Number: n” inside the BODY tag
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """Display an HTML page only if n is an integer.
+    H1 tag: “Number: n is even|odd” inside the BODY tag
+    """
+    parity = "even" if n % 2 == 0 else "odd"
+    return render_template('number_odd_or_even.html', n=n, parity=parity)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
